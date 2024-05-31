a49e1b6485150ee80faafcfa0547b9f3c145db5a
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-31 17:08:32 -0300

    alter page for Details

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/global";
-import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components"
+import GlobalStyles from "./styles/global"
+import theme from "./styles/theme"
 
-import { Home } from "./pages/Home";
+import { Details } from "./pages/Details"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>
-);
+)
