2ada7c440ddc2f18cbf2b1c751160236332e490c
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-03 10:39:17 -0300

    feat: created structure Profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+
+import { ButtonText } from "../../components/ButtonText"
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <ButtonText title="Voltar" />
+      </header>
+    </Container>
+  )
+}
