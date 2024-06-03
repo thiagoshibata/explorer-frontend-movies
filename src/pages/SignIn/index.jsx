e66e5d4129df575aecefd38ada848a571274e154
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-03 16:03:21 -0300

    Alter text in form

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -8,8 +8,8 @@ export function SignIn() {
   return (
     <Container>
       <Form>
-        <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Faça seu login</h2>
         <Input icon={FiMail} type="text" placeholder="E-mail" />
         <Input icon={FiLock} type="password" placeholder="Senha" />
